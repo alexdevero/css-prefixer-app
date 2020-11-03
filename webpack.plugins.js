50bfeaea3acc745d8549ddd68343636d563f95c9
Alex Devero <deveroalex@gmail.com>
2020-11-03 05:18:54 +0100

    Add MonacoWebpackPlugin, CopyWebpackPlugin

--- webpack.plugins.js
+++ webpack.plugins.js
@@ -1,5 +1,18 @@
+const path = require('path')
+
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
 
 module.exports = [
-  new ForkTsCheckerWebpackPlugin()
+  new ForkTsCheckerWebpackPlugin(),
+  new MonacoWebpackPlugin(),
+  new CopyWebpackPlugin({
+    patterns: [
+      {
+        from: path.resolve(__dirname, 'src', 'images'),
+        to: path.resolve(__dirname, '.webpack/main', 'images')
+      }
+    ]
+  })
 ]
