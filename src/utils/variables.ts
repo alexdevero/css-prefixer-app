326974864afd1dc964f89d5c32e8550d41624c86
Alex Devero <deveroalex@gmail.com>
2020-11-04 15:12:35 +0100

    Refactor variables

---
+++ src/utils/variables.ts
@@ -0,0 +1,8 @@
+// Cache HTML elements
+export const processBtn: HTMLButtonElement = document.querySelector('#process-btn')
+export const prettifyBtn: HTMLButtonElement = document.querySelector('#prettify-btn')
+export const inputCSSBox: HTMLTextAreaElement = document.querySelector('#input-css')
+export const inputCSSBoxMonaco: HTMLTextAreaElement = document.querySelector('#input-css .view-lines')
+export const resultBox: HTMLTextAreaElement = document.querySelector('#result-box')
+export const resultBoxMonaco: HTMLTextAreaElement = document.querySelector('#result-box .view-lines')
+export const browserlistInput: HTMLInputElement = document.querySelector('#browserlist-input')
